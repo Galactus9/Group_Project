891d07ee56e67be18c8b4d59aeadd4a2caf26a99
Galactus9 <117937168+Galactus9@users.noreply.github.com>
2023-01-14 15:46:35 +0200

    image ok

--- Group_Project/Controllers/QuestionController.cs
+++ Group_Project/Controllers/QuestionController.cs
@@ -4,9 +4,32 @@ namespace Group_Project.Controllers
 {
 	public class QuestionController : Controller
 	{
-		public IActionResult Index()
+
+
+        private readonly IWebHostEnvironment _env;
+            public QuestionController(IWebHostEnvironment env)
+            {
+            _env = env;
+            }
+
+        public IActionResult Index()
 		{
 			return View();
+		}
+		[HttpPost]
+		public ActionResult UploadImage(List<FormFile> files)
+		{
+			var filePath = "";
+			foreach(IFormFile photo in Request.Form.Files)
+			{
+				string serverMapPath = Path.Combine(_env.WebRootPath , "Image", photo.FileName);
+				using (var stream = new FileStream( serverMapPath, FileMode.Create))
+				{
+					photo.CopyTo(stream);
+				}
+				filePath = "https://localhost:44367/" + "Image/" + photo.FileName;
+			}
+			return Json(new { url = filePath});
 		}
 		public IActionResult Create()
 		{
